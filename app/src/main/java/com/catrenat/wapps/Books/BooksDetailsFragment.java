8a71ff026edace4224cc9491f19bdf5392a67a03
Edwing <edwingmontero82@gmail.com>
2022-01-24 05:56:18 +0100

    [WP-32] Added background in DetailFragment and 3 information planes

--- app/src/main/java/com/catrenat/wapps/Books/BooksDetailsFragment.java
+++ app/src/main/java/com/catrenat/wapps/Books/BooksDetailsFragment.java
@@ -33,6 +33,8 @@ public class BooksDetailsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_book_details, container, false);
 
+
+
         return view;
     }
 }
